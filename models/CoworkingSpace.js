bf72a42b274ef1d37e98f78230502cf49ecf40ad
BoondaPanich <nrboonda@gmail.com>
2024-03-04 11:33:29 +0700

    coWorking edited

--- models/CoworkingSpace.js
+++ models/CoworkingSpace.js
@@ -37,7 +37,7 @@ const CoworkingSpaceSchema = new mongoose.Schema({
 
     CoworkingSpaceSchema.pre('deleteOne', {document: true, query: false}, async function(next) {
         console.log(`Reservations being removed from Co-working space ${this._id}`);
-        await this.model('Reservation').deleteMany({coWorking: this._id});
+        await this.model('Reservation').deleteMany({coworkingSpace: this._id});
         next();
     });
 
