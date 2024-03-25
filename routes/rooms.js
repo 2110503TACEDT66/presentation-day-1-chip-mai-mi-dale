a3f537e08698ddd8b520457a0c4e11e3eaa6f7f7
Yiwahpsp <Patsaporn.Kaewkong@gmail.com>
2024-03-26 01:15:56 +0700

    ver 2

--- routes/rooms.js
+++ routes/rooms.js
@@ -8,16 +8,12 @@ const {
 } = require("../controllers/rooms");
 
 const router = express.Router({ mergeParams: true });
-
 const { protect, authorize } = require("../middleware/auth");
 
-router
-  .route("/")
-  .get(protect, getRooms)
-  .post(protect, authorize("admin"), createRoom);
+router.route("/").get(getRooms).post(protect, authorize("admin"), createRoom);
 router
   .route("/:id")
-  .get(protect, getRoom)
+  .get(getRoom)
   .put(protect, authorize("admin"), updateRoom)
   .delete(protect, authorize("admin"), deleteRoom);
 
