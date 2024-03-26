c84498dc89242a79bac292b38886a73268f24100
panisarac <panisara.chk@gmail.com>
2024-03-26 20:45:26 +0700

    vercel.json added

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ const swaggerOptions={
         },
         servers:[
             {
-                url: 'http://localhost:5000/api/v1'
+                url: process.env.HOST + ':' + PORT + '/api/v1'
             }
         ],
     },
@@ -81,7 +81,7 @@ app.use('/api/v1/rooms', rooms);
 app.use('/api/v1/auth', auth);
 
 const PORT=process.env.PORT || 5000;
-const server = app.listen(PORT, console.log('Server running in', process.env.NODE_ENV, 'mode on port', PORT));
+const server = app.listen(PORT, console.log('Server running in', process.env.NODE_ENV,"on" + process.env.HOST + ':'+ PORT));
 
 //Handle unhandled promise rejections
 process.on('unhandledRejection', (err, promise) => {
