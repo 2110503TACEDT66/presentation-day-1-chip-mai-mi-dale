d9a97939fe4670d398780d39b7dae89486f74b5f
panisarac <panisara.chk@gmail.com>
2024-03-28 12:48:07 +0700

    edit send token req

--- server.js
+++ server.js
@@ -26,6 +26,9 @@ const auth = require('./routes/auth');
 
 const app = express();
 
+const PORT=process.env.PORT || 5000;
+const server = app.listen(PORT, console.log('Server running in', process.env.NODE_ENV,"on" + process.env.HOST + ':' + PORT));
+
 const swaggerOptions={
     swaggerDefinition:{
         openapi: '3.0.0',
@@ -79,9 +82,6 @@ app.use('/api/v1/coworkingSpaces', coworkingSpaces);
 app.use('/api/v1/reservations', reservations);
 app.use('/api/v1/rooms', rooms);
 app.use('/api/v1/auth', auth);
-
-const PORT=process.env.PORT || 5000;
-const server = app.listen(PORT, console.log('Server running in', process.env.NODE_ENV,"on" + process.env.HOST + ':'+ PORT));
 
 //Handle unhandled promise rejections
 process.on('unhandledRejection', (err, promise) => {
